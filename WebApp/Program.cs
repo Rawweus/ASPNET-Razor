bf6bc121a309ac148acf7d49f545f71d7d0c8b78
Robin Kareem <robin.r.kareem@gmail.com>
2024-02-19 21:03:07 +0100

    Add project files.

---
+++ WebApp/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args); // Denna bygger en webserver
+builder.Services.AddRazorPages(); //För att den ska veta hur den ska hantera sidorna
+
+
+
+
+
+
+
+
+var app = builder.Build(); // Den ska trigga och bygga Build-delen i "app" så det blir en web application
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.Run();
