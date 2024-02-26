d3e0950c3cc47742ff6e923144463c9ca5586f12
Robin Kareem <robin.r.kareem@gmail.com>
2024-02-26 21:14:36 +0100

    signinformmodel

---
+++ WebApp/Models/SignInFormModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models;
+
+public class SignInFormModel
+{
+	[DataType(DataType.EmailAddress)]
+	[Display(Name = "Email", Prompt = "Enter your email address")]
+	[Required(ErrorMessage = "Enter a valid email address")]
+
+	public string Email { get; set; } = null!;
+
+	[DataType(DataType.Password)]
+	[Display(Name = "Password", Prompt = "Enter your password")]
+	[Required(ErrorMessage = "Enter a valid password")]
+
+	public string Password { get; set; } = null!;
+
+	[Display(Name = "Remember me")]
+	public bool RememberMe { get; set; } = false;
+}
